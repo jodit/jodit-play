24456260cab48a407ca45f194a9336341a07600f
Chupurnov Valeriy <skoder@ya.ru>
2018-02-13 21:23:57 +0500

    New version 0.1.19. Read more https://github.com/xdan/jodit/releases/tag/0.1.19

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-window.JoditPlayConfig.ready = (element) => {
+window.JoditPlayReady = (element) => {
     ReactDOM.render(<App />, element);
     registerServiceWorker();
 };
 
-let element = document.getElementById('root');
-element && window.JoditPlayConfig.ready(element);
+const element = document.getElementById('root');
+element && window.JoditPlayReady(element);
 
