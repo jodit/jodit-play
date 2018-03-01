59669a902898106332eec2c727dcd8a570972e07
Chupurnov Valeriy <skoder@ya.ru>
2018-03-01 20:34:40 +0500

    New version 0.1.33. Read more https://github.com/xdan/jodit/releases/tag/0.1.33

--- src/components/plugins/Plugins.js
+++ src/components/plugins/Plugins.js
@@ -6,16 +6,18 @@ import CheckBox from "../checkbox/CheckBox";
 export default class Plugins extends Component {
     togglePlugin = (value, pluginName) => {
         let plugins = Array.isArray(this.props.config.disablePlugins) ? [...this.props.config.disablePlugins] : [...this.props.config.disablePlugins.split(/[\s\,]+/)];
-
+        plugins = plugins.filter(a => a);
 
         if (plugins.indexOf(pluginName) === -1 && !value) {
             plugins.push(pluginName);
         }
+
         if (plugins.indexOf(pluginName) !== -1 && value) {
             plugins.splice(plugins.indexOf(pluginName), 1);
         }
+
         if (plugins.toString() !== this.props.config.disablePlugins.toString()) {
-            this.props.setOption(plugins, 'disablePlugins');
+            this.props.setOption(plugins.length ? plugins.join(',') : [], 'disablePlugins');
         }
     };
 
