29911cb1df0aef9ab88949d908abcbc07bae26bd
Chupurnov Valeriy <skoder@ya.ru>
2018-04-03 14:11:40 +0500

    New version 0.1.46. Read more https://github.com/jodit/jodit-docs/releases/tag/0.1.46

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+if (!window.JoditPlayConfig) {
+    window.JoditPlayConfig = {};
+}
+
 window.JoditPlayReady = (element) => {
     ReactDOM.render(<App />, element);
 };
