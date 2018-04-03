365e7011da9481ff8db2f0f46bfad56e8f26a206
Chupurnov Valeriy <skoder@ya.ru>
2018-04-03 12:28:50 +0500

    New version 0.1.39. Read more https://github.com/jodit/jodit-docs/releases/tag/0.1.39

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+
 // import createHistory from 'history/createBrowserHistory'
 // const history = createHistory()
 
 window.JoditPlayReady = (element) => {
     ReactDOM.render(<App />, element);
-    registerServiceWorker();
 };
 
 const element = document.getElementById('root');
