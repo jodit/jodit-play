0f075e206365618ed393f04c803b92252082c9d3
Valera <v-chupurnov@edadeal.ru>
2020-02-07 20:37:16 +0300

    New version 0.1.66. Read more https://github.com/jodit/jodit-docs/releases/tag/0.1.66

--- src/components/plugins/Plugins.js
+++ src/components/plugins/Plugins.js
@@ -1,42 +1,45 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import style from './style.module.css';
 import Jodit from 'jodit';
 import CheckBox from "../checkbox/CheckBox";
 
 export default class Plugins extends Component {
-    togglePlugin = (value, pluginName) => {
-        let plugins = Array.isArray(this.props.config.disablePlugins) ? [...this.props.config.disablePlugins] : [...this.props.config.disablePlugins.split(/[\s,]+/)];
-        plugins = plugins.filter(a => a);
+	togglePlugin = (value, pluginName) => {
+		let plugins = Array.isArray(this.props.config.disablePlugins) ?
+      [...this.props.config.disablePlugins] :
+      [...this.props.config.disablePlugins.split(/[\s,]+/)];
 
-        if (plugins.indexOf(pluginName) === -1 && !value) {
-            plugins.push(pluginName);
-        }
+		plugins = plugins.filter(Boolean);
 
-        if (plugins.indexOf(pluginName) !== -1 && value) {
-            plugins.splice(plugins.indexOf(pluginName), 1);
-        }
+		if (plugins.indexOf(pluginName) === -1 && !value) {
+			plugins.push(pluginName);
+		}
 
-        if (plugins.toString() !== this.props.config.disablePlugins.toString()) {
-            this.props.setOption(plugins.length ? plugins.join(',') : [], 'disablePlugins');
-        }
-    };
+		if (plugins.indexOf(pluginName) !== -1 && value) {
+			plugins.splice(plugins.indexOf(pluginName), 1);
+		}
 
-    render() {
-        const plugins = Object.keys(Jodit.plugins).map((pluginName) => {
-            return <CheckBox
-                popupKey={'plugins/' + pluginName}
-                key={pluginName}
-                name={pluginName}
-                onChange={this.togglePlugin}
-                defaultChecked={this.props.config.disablePlugins.indexOf(pluginName) === -1}
-                label={pluginName}
-            />;
-        });
+		if (plugins.toString() !== this.props.config.disablePlugins.toString()) {
+			this.props.setOption(plugins.length ? plugins.join(',') : [], 'disablePlugins');
+		}
+	};
 
-        return (
-            <div className={style.main}>
-                {plugins}
-            </div>
-        );
-    }
+	render() {
+		const plugins = Array.from(Jodit.plugins.items.keys()).map((pluginName) =>
+			<CheckBox
+				popupKey={'plugins/' + pluginName}
+				key={pluginName}
+				name={pluginName}
+				onChange={this.togglePlugin}
+				defaultChecked={this.props.config.disablePlugins.indexOf(pluginName) === -1}
+				label={pluginName}
+			/>
+		);
+
+		return (
+			<div className={style.main}>
+				{plugins}
+			</div>
+		);
+	}
 }
