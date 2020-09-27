526bee5c6adeb760473cb6cdc18d34c9f04a1e97
v-chupurnov@gmail.com <skoder@ya.ru>
2020-09-27 19:26:04 +0300

    New version 0.1.86. Read more https://github.com/jodit/jodit-docs/releases/tag/0.1.86

---
+++ build/precache-manifest.fb3a4dad4503e576f4fba36225dad28f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a209bcf682b662c30368914f6c11190",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cd8832d733f3d477a51e",
+    "url": "./static/css/2.ad109a12.chunk.css"
+  },
+  {
+    "revision": "6d8f345b1dccb6127675",
+    "url": "./static/css/main.4409c2ad.chunk.css"
+  },
+  {
+    "revision": "cd8832d733f3d477a51e",
+    "url": "./static/js/2.a4585dd8.chunk.js"
+  },
+  {
+    "revision": "6f83b2ee0a50daf4b71e38a002c6eaba",
+    "url": "./static/js/2.a4585dd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d8f345b1dccb6127675",
+    "url": "./static/js/main.e76c6b71.chunk.js"
+  },
+  {
+    "revision": "f6ef612a21973d1a212a",
+    "url": "./static/js/runtime-main.5cf2a895.js"
+  },
+  {
+    "revision": "0bfc1b71181933e123c97c779f47a7d1",
+    "url": "./static/media/add.0bfc1b71.svg"
+  },
+  {
+    "revision": "2ed481513be263ee400e8a8d40b25878",
+    "url": "./static/media/down.2ed48151.svg"
+  },
+  {
+    "revision": "4397bbb73991531edb0c911e11abfe30",
+    "url": "./static/media/resore.4397bbb7.svg"
+  },
+  {
+    "revision": "9cb77013a5aca6d6205b7c203cc2161d",
+    "url": "./static/media/trash.9cb77013.svg"
+  },
+  {
+    "revision": "72d8af398f221d051a5b86678bba85aa",
+    "url": "./static/media/up.72d8af39.svg"
+  }
+]);
